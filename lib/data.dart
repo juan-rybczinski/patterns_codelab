0eb3fc6ad789c3699ea41e4f7c62989f71075b84
juan-rybczinski <jhreplay.lee@gmail.com>
2023-05-21 23:33:29 +0900

    [Dart 3.0] 이전 문법으로 구현

--- lib/data.dart
+++ lib/data.dart
@@ -1,8 +1,91 @@
 import 'dart:convert';
 
+class MetaData {
+  final String title;
+  final DateTime modified;
+
+  MetaData({
+    required this.title,
+    required this.modified,
+  });
+}
+
+class Block {
+  Block();
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json.containsKey('type') && json.containsKey('text')) {
+      if (json['type'] == 'h1') {
+        return HeaderBlock(text: json['text']);
+      } else if (json['type'] == 'p') {
+        return ParagraphBlock(text: json['text']);
+      } else if (json['type'] == 'checkbox' && json.containsKey('checked')) {
+        return CheckboxBlock(text: json['text'], isChecked: json['checked']);
+      } else {
+        throw const FormatException('Unexpected JSON format');
+      }
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+class HeaderBlock extends Block {
+  final String text;
+
+  HeaderBlock({
+    required this.text,
+  });
+}
+
+class ParagraphBlock extends Block {
+  final String text;
+
+  ParagraphBlock({required this.text});
+}
+
+class CheckboxBlock extends Block {
+  final String text;
+  final bool isChecked;
+
+  CheckboxBlock({
+    required this.text,
+    required this.isChecked,
+  });
+}
+
 class Document {
   final Map<String, Object?> _json;
+
   Document() : _json = jsonDecode(documentJson);
+
+  MetaData getMetadata() {
+    if (_json.containsKey('metadata')) {
+      var metadataJson = _json['metadata'];
+      if (metadataJson is Map) {
+        var title = metadataJson['title'] as String;
+        var localModified = DateTime.parse(metadataJson['modified'] as String);
+        return MetaData(
+          title: title,
+          modified: localModified,
+        );
+      }
+    }
+    throw const FormatException('Unexpected JSON');
+  }
+
+  List<Block> getBlocks() {
+    if (_json.containsKey('blocks')) {
+      final blocksJson = _json['blocks'];
+      if (blocksJson is List) {
+        return blocksJson.map((block) => Block.fromJson(block)).toList();
+      } else {
+        throw const FormatException('Unexpected JSON format');
+      }
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
 }
 
 const documentJson = '''
--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,98 @@ class DocumentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final metadata = document.getMetadata();
+    final formattedModifiedDate = formatDate(metadata.modified);
+    final blocks = document.getBlocks();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title goes here'),
+        title: Text(metadata.title),
       ),
       body: Column(
         children: [
-          Center(
-            child: Text('Body goes here'),
+          Text('Last modified $formattedModifiedDate'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) => BlockWidget(
+                block: blocks[index],
+              ),
+            ),
           ),
         ],
       ),
     );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (block is HeaderBlock) {
+      final headerBlock = block as HeaderBlock;
+      return Container(
+        margin: const EdgeInsets.all(8.0),
+        child: Text(
+          headerBlock.text,
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      );
+    } else if (block is ParagraphBlock) {
+      final paragraphBlock = block as ParagraphBlock;
+      return Container(
+        margin: const EdgeInsets.all(8.0),
+        child: Text(paragraphBlock.text),
+      );
+    } else if (block is CheckboxBlock) {
+      final checkboxBlock = block as CheckboxBlock;
+      return Container(
+        margin: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Checkbox(value: checkboxBlock.isChecked, onChanged: (_) {}),
+            Text(checkboxBlock.text),
+          ],
+        ),
+      );
+    } else {
+      throw Exception('Unexpected block');
+    }
+
+    // switch (block.type) {
+    //   case 'h1':
+    //     textStyle = Theme.of(context).textTheme.displayMedium;
+    //     break;
+    //   case 'p':
+    //   case 'checkbox':
+    //     textStyle = Theme.of(context).textTheme.bodyMedium;
+    //     break;
+    //   default:
+    //     textStyle = Theme.of(context).textTheme.bodySmall;
+    // }
+  }
+}
+
+String formatDate(DateTime dateTime) {
+  var today = DateTime.now();
+  var difference = dateTime.difference(today);
+
+  if (difference.inDays == 0) {
+    return 'today';
+  } else if (difference.inDays == 1) {
+    return 'tomorrow';
+  } else if (difference.inDays == -1) {
+    return 'yesterday';
+  } else if (difference.isNegative) {
+    return '${difference.inDays.abs()} days ago';
+  } else {
+    return '${difference.inDays} days from now';
   }
 }
